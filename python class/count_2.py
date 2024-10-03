6be4fb5ed0fc1c5ab30bbc5a471e4d63dc9f61c1
Gurpreetsingh2161 <Gurpreet.14571@stu.upes.ac.in>
2024-10-03 15:56:59 +0530

    Changes on 3/10/2024(3)

---
+++ python class/count_2.py
@@ -0,0 +1,29 @@
+import time
+
+n = 100000
+
+def wrapper(func,*args,**kwargs): #generalize
+    def wrapped(*args,**kwargs):
+        start_time=time.time() * 1000000
+        func(*args,**kwargs) #timing this function call/execution
+        end_time=time.time() * 1000000
+        print(f"\n n = {n} Time to execute is {end_time - start_time} micro seconds")
+
+    # wrapped(*args,**kwargs)
+    return wrapped
+
+@wrapper #decorator
+def count(n):
+    for i in range(0,n):
+        a = n * 10
+
+count(n)
+
+@wrapper
+def random():
+    pass
+
+random()
+#wrapped_function=wrapper(count,n)
+#wrapped_count= wrapper(count,n)
+#wrapped_count(n)
